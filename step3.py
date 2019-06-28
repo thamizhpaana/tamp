c28c27c23da4162a4e89e20368fc3b89c85101b7
thamizhpaana <52271356+thamizhpaana@users.noreply.github.com>
2019-06-28 11:30:22 +0530

    Create step3.py

---
+++ step3.py
@@ -0,0 +1,9 @@
+ch = input( )
+ 
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print("Vowel")
+elif(ch=='b' or ch=='c' or ch=='d' or ch=='f' or ch=='g' or ch=='h' or ch=='j' or ch=='k' or ch=='l'):
+	print("Consonant")
+else:
+    print("invalide")
